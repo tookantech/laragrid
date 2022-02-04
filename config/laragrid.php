74434a6a30b3d22f02b141f2dfefe3c8cf14207e
aryala7 <arya.lavasani@gmail.com>
2022-02-04 19:50:03 +0330

    [fix] fix service provider bug

--- config/laragrid.php
+++ config/laragrid.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-        'laragrid'=>[
-            'main_net'=>'https://api.trongrid.io',
-            'shasta_testnet' => 'https://api.shasta.trongrid.io',
-            'nile_testnet' => 'https://nile.trongrid.io',
-        ],
+        'main_net'=>'https://api.trongrid.io',
+        'shasta_testnet' => 'https://api.shasta.trongrid.io',
+        'nile_testnet' => 'https://nile.trongrid.io',
         'token' => env('LARAGRID_TOKEN', 'trongrid'),
     ];
